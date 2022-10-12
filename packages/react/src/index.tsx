3806a6679cfc08384b29b03719f87b7d49a01dd2
Matheus Pires <matheushenriquepires99@gmail.com>
2022-10-12 14:11:54 -0300

    feat: Setup Storybook

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,7 +1,11 @@
 import { styled } from "./styles";
 
 const Button = styled("button", {
-  fontFamily: "",
+  fontFamily: "$default",
+  background: "$ignite500",
+  borderRadius: "$md",
+  padding: "$4",
+  height: "$10",
 });
 
 export const App = () => {
