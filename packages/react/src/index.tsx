2cb000169b42ddd30a88c81152bb2c621ade8eea
Matheus Pires <matheushenriquepires99@gmail.com>
2022-10-16 10:48:38 -0300

    chore: Setup Storybook

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,13 +1,9 @@
 import { styled } from "./styles";
 
-const Button = styled("button", {
+export const Button = styled("button", {
   fontFamily: "$default",
   background: "$ignite500",
   borderRadius: "$md",
   padding: "$4",
   height: "$10",
 });
-
-export const App = () => {
-  return <Button>Hello World</Button>;
-};
