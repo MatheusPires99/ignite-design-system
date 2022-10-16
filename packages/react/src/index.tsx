9c9985d3984eb5a3cdea66b03750150edfe316c6
Matheus Pires <matheushenriquepires99@gmail.com>
2022-10-16 10:58:26 -0300

    feat: Start button stories

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,9 +1,33 @@
+import { ComponentProps } from "react";
+
 import { styled } from "./styles";
 
 export const Button = styled("button", {
   fontFamily: "$default",
-  background: "$ignite500",
+  fontWeight: "bold",
+  color: "$white",
+  background: "$ignite300",
   borderRadius: "$md",
-  padding: "$4",
   height: "$10",
+  border: 0,
+
+  variants: {
+    size: {
+      small: {
+        fontSize: 14,
+        padding: "$2 $4",
+      },
+
+      big: {
+        fontSize: 16,
+        padding: "$3 $6",
+      },
+    },
+  },
+
+  defaultVariants: {
+    size: "small",
+  },
 });
+
+export type ButtonProps = ComponentProps<typeof Button>;
