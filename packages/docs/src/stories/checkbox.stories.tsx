4d8d0e72d06a2f7608c92a23a27215e9d67b9b8e
Matheus Pires <matheushenriquepires99@gmail.com>
2022-10-17 15:20:25 -0300

    feat: Create Checkbox component

---
+++ packages/docs/src/stories/checkbox.stories.tsx
@@ -0,0 +1,17 @@
+import { Box, Text, Checkbox, CheckboxProps } from "@ignite-ui/react";
+import type { StoryObj, Meta } from "@storybook/react";
+
+export default {
+  title: "Form/Checkbox",
+  component: Checkbox,
+  decorators: [
+    (Story) => (
+      <Box as="label" css={{ display: "flex", gap: "$2" }}>
+        {Story()}
+        <Text size="sm">Accept terms of use</Text>
+      </Box>
+    ),
+  ],
+} as Meta<CheckboxProps>;
+
+export const Default: StoryObj<CheckboxProps> = {};
