3365c8458264fcc3b4940172b457b5fd099cd91a
Matheus Pires <matheushenriquepires99@gmail.com>
2022-10-17 14:53:26 -0300

    feat: Create Button component

---
+++ packages/docs/src/stories/avatar.stories.tsx
@@ -0,0 +1,19 @@
+import { Avatar, AvatarProps } from "@ignite-ui/react";
+import type { StoryObj, Meta } from "@storybook/react";
+
+export default {
+  title: "Data Display/Avatar",
+  component: Avatar,
+  args: {
+    src: "https://github.com/MatheusPires99.png",
+    alt: "Matheus Pires",
+  },
+} as Meta<AvatarProps>;
+
+export const Default: StoryObj<AvatarProps> = {};
+
+export const WithFallback: StoryObj<AvatarProps> = {
+  args: {
+    src: undefined,
+  },
+};
